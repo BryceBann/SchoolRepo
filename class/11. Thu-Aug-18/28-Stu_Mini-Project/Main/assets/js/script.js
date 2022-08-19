f848148489e8315ebbb950900c563a86956d1b2b
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-08-19 00:01:12 -0500

    last push of class

--- class/11. Thu-Aug-18/28-Stu_Mini-Project/Main/assets/js/script.js
+++ class/11. Thu-Aug-18/28-Stu_Mini-Project/Main/assets/js/script.js
@@ -3,3 +3,33 @@ var win = document.querySelector(".win");
 var lose = document.querySelector(".lose");
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
+var wins = 0;
+var losses = 0;
+
+function setLossesText(){
+    lose.textContent = losses
+    localStorage.getItem
+}
+
+startButton.addEventListener("click", function(){
+    var timeLeft = 10;
+    
+    
+    var timeInterval = setInterval(function (){
+        timerElement.innerHTML = timeLeft;
+
+        timeLeft -= 1;
+        if (timeLeft <= -1){
+            clearInterval(timeInterval);
+            timerElement.innerHTML = "time is up!"
+            losses++
+            localStorage.setItem("lose", losses)
+            setLossesText();
+            
+        } 
+
+    
+    }, 1);
+})
+
+
