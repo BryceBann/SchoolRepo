dd87df618dddd10a8c999fac4d974bf003c198ec
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-08-22 21:42:48 -0500

    last push of class

--- class/12 . Mon-Aug-22/02-Stu_jQuery-Elements/Unsolved/assets/js/script.js
+++ class/12 . Mon-Aug-22/02-Stu_jQuery-Elements/Unsolved/assets/js/script.js
@@ -1,16 +1,18 @@
 // TODO: Select the existing DOM element, <div id="root">, and assign to a new variable
 // Hint: Visit the documentation https://api.jquery.com/
-
+var rootEl = $('#root');
 // TODO: Create a `<p>` element that will store an author's name and assign that created element to a new variable
-
+var aurthorEl = $('<p>');
 // TODO: Add the following text to the newly created `<p>` element: "~ Carol Dweck"
-
+aurthorEl.text('')
 // TODO: Add the class `plain` to the author element
-
+aurthorEl.addClass('plain')
 // TODO: Create a new `<h1>` element that will store an author's quote and assign it to new variable
-
+var quoteEl = $('<h1>')
 // TODO: Add the following quote text to the quote element, "Love Challenges, Be Intrigued by Mistakes, Enjoy Effort, and Keep Learning."
-
+quoteEl.text(
+    'blah blah blah'
+)
 // TODO: Apply the class `fancy` to the quote element
 
 // TODO: Append the author element to the quote element
--- class/12 . Mon-Aug-22/06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ class/12 . Mon-Aug-22/06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -2,5 +2,17 @@ var shoppingFormEl = $('#shopping-form');
 var shoppingListEl = $('#shopping-list');
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
+function handleSubmit(event) {
+    event.preventDefault();
 
+    var shoppingItem = $('input[name="shopping-input"]').val();
+
+    if(!shoppingItem) {
+        console.log('no shoping item found');
+        return;
+    }
+    shoppingListEl.append('<li>' + shoppingItem + '</li>');
+    $('input[name="shopping-input"]').val('');
+}
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+shoppingFormEl.on('submit',handleSubmit);
