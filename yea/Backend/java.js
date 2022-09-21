141bdbd879423d5830080c7cc51ad44a383da24a
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-09-21 16:20:49 -0500

    redo on practice page

---
+++ yea/Backend/java.js
@@ -0,0 +1,45 @@
+function saveArtist(){
+    artist = $("#artistInput").val()
+    console.log(artist)
+    }
+    
+    
+    
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '3d1c2d7f67mshddf82a9182fa081p19577ajsn1520b3beb5fa',
+// 		'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://spotify81.p.rapidapi.com/tracks', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+//     const choice = {
+//         method: 'GET',
+//         headers: {
+//             'X-RapidAPI-Key': '3d1c2d7f67mshddf82a9182fa081p19577ajsn1520b3beb5fa',
+//             'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+//         }
+//     };
+    
+//     fetch('https://spotify81.p.rapidapi.com/top_200_tracks', choice)
+//         .then(response => response.json())
+//         .then(response => console.log(response))
+//         .catch(err => console.error(err));
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '3d1c2d7f67mshddf82a9182fa081p19577ajsn1520b3beb5fa',
+// 		'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://spotify81.p.rapidapi.com/artist_related?id=2w9zwq3AktTeYYMuhMjju8', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
