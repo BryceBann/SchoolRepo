a32f60db68e8eca46c9adbc66ed1eb861d0e4f67
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-09-22 18:28:28 -0500

    small changes

--- yea/Backend/java.js
+++ yea/Backend/java.js
@@ -1,9 +1,22 @@
 function saveArtist(){
     artist = $("#artistInput").val()
     console.log(artist)
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3d1c2d7f67mshddf82a9182fa081p19577ajsn1520b3beb5fa',
+            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+        }
+    };
+    
+    fetch(`https://spotify81.p.rapidapi.com/artist?${artist}`, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
     }
     
-    
+    $("#search-button").on("click",saveArtist)
     
 
 // const options = {
@@ -39,7 +52,7 @@ function saveArtist(){
 // 	}
 // };
 
-// fetch('https://spotify81.p.rapidapi.com/artist_related?id=2w9zwq3AktTeYYMuhMjju8', options)
+// fetch('https://spotify81.p.rapidapi.com/artist_related?id=${artist}', options)
 // 	.then(response => response.json())
 // 	.then(response => console.log(response))
 // 	.catch(err => console.error(err));
