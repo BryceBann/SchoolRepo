bb5d110853d7e702c80076039b141ca88f21680f
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-09-23 08:44:48 -0500

    finished with class work

--- class/19. Sept-22/UnSolved/20-Stu_Inquirer-Users/index.js
+++ class/19. Sept-22/UnSolved/20-Stu_Inquirer-Users/index.js
@@ -1,20 +1,31 @@
 const inquirer = require('inquirer');
 const fs = require('fs')
+
 inquirer
   .prompt([
     {
    type: 'input',
    name: 'name',
-   messgae: "what is your name"
+   messgae: "what is your name",
     },
     {
-        type: 'input',
-        name: 'name',
-        messgae: "What languages do you know?"
+        type: 'checkbox',
+        messgae: "What languages do you know?",
+        name: 'stack',
+        choices: ['HTML', 'CSS', 'Javascript', 'MySql']
          },
          {
-            type: 'input',
-            name: 'name',
-            messgae: "What is your preferred method of communication?"
+            type: 'list',
+            messgae: "What is your preferred method of communication?",
+            name: 'contact',
+            choices: ['email','phone', 'telekinesis']
+           
              }
-  ]);
+  ])
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
