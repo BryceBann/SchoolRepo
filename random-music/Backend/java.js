d902d7aea7f3cedd02c98de019e613a70b1d4a9c
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-09-25 13:21:21 -0500

    small changes to HTML and java

--- random-music/Backend/java.js
+++ random-music/Backend/java.js
@@ -1,19 +1,20 @@
 function saveArtist(){
     artist = $("#artistInput").val()
     console.log(artist)
+    $("#searchedArtist").text(" " + artist)
 
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '3d1c2d7f67mshddf82a9182fa081p19577ajsn1520b3beb5fa',
-            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
-        }
-    };
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         'X-RapidAPI-Key': '3d1c2d7f67mshddf82a9182fa081p19577ajsn1520b3beb5fa',
+    //         'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+    //     }
+    // };
     
-    fetch(`https://spotify81.p.rapidapi.com/artist?${artist}`, options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+    // fetch(`https://spotify81.p.rapidapi.com/artist?${artist}`, options)
+    //     .then(response => response.json())
+    //     .then(response => console.log(response))
+    //     .catch(err => console.error(err));
     }
     
     $("#search-button").on("click",saveArtist)
