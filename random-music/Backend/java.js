896b9577adf046ac69b90a3a93cbe9e91fe45e00
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-09-29 13:37:41 -0500

    no real change

--- random-music/Backend/java.js
+++ random-music/Backend/java.js
@@ -3,57 +3,20 @@ function saveArtist(){
     console.log(artist)
     $("#searchedArtist").text(" " + artist)
 
-    // const options = {
-    //     method: 'GET',
-    //     headers: {
-    //         'X-RapidAPI-Key': '3d1c2d7f67mshddf82a9182fa081p19577ajsn1520b3beb5fa',
-    //         'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
-    //     }
-    // };
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3d1c2d7f67mshddf82a9182fa081p19577ajsn1520b3beb5fa',
+            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+        }
+    };
     
-    // fetch(`https://spotify81.p.rapidapi.com/artist?${artist}`, options)
-    //     .then(response => response.json())
-    //     .then(response => console.log(response))
-    //     .catch(err => console.error(err));
+    fetch(`https://spotify81.p.rapidapi.com/artist?${artist}`, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
     }
     
     $("#search-button").on("click",saveArtist)
     
 
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '3d1c2d7f67mshddf82a9182fa081p19577ajsn1520b3beb5fa',
-// 		'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
-// 	}
-// };
-
-// fetch('https://spotify81.p.rapidapi.com/tracks', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-
-//     const choice = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Key': '3d1c2d7f67mshddf82a9182fa081p19577ajsn1520b3beb5fa',
-//             'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
-//         }
-//     };
-    
-//     fetch('https://spotify81.p.rapidapi.com/top_200_tracks', choice)
-//         .then(response => response.json())
-//         .then(response => console.log(response))
-//         .catch(err => console.error(err));
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '3d1c2d7f67mshddf82a9182fa081p19577ajsn1520b3beb5fa',
-// 		'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
-// 	}
-// };
-
-// fetch('https://spotify81.p.rapidapi.com/artist_related?id=${artist}', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
