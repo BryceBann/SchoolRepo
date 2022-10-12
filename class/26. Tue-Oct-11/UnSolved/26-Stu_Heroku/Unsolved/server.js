d70201cd8fc847a528be4dfe766f84d0477f1c99
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-10-12 13:18:45 -0500

    after

--- class/26. Tue-Oct-11/UnSolved/26-Stu_Heroku/Unsolved/server.js
+++ class/26. Tue-Oct-11/UnSolved/26-Stu_Heroku/Unsolved/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -30,3 +30,12 @@ app.get('/feedback', (req, res) =>
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
+// git comands for heroku
+// heroku auth:login -will ask to press any key and will opne webpage for login
+// git init -will initialize a empty git repo
+// git add . -will add to git repo
+// git commit -m "" -will commit to repo
+// heroku dreate -will create random url
+// git remote -v -not to sure
+// git push heroku main -will push to heroku may not work use comand line below does same
+// git push heroku HEAD:main
