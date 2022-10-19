a5038c71ee0aab9c12c34d1808315b181d115b49
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-10-18 20:47:33 -0500

    push to pull mini

--- class/29. Tue-Oct-18/UnSolved/24-Stu_COUNT-GROUPBY/Unsolved/server.js
+++ class/29. Tue-Oct-18/UnSolved/24-Stu_COUNT-GROUPBY/Unsolved/server.js
@@ -17,11 +17,11 @@ const db = mysql.createConnection(
   console.log(`Connected to the books_db database.`)
 );
 
-
+//looking into the favorite_books table and grouping the boks together of in_stock
 db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
   console.log(results);
 });
-
+//making a new column total_in_section to show number of books in section will show the max and min number of boks in section 
 db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
   console.log(results);
 });
