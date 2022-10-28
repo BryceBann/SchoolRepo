9e98e57f390cbe272b67226d9dedf08b81db9024
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-10-27 21:36:35 -0500

    final push of the day

--- class/33. Thu-Oct-27/08-Stu_Data-Serialization/Unsolved/controllers/dish-routes.js
+++ class/33. Thu-Oct-27/08-Stu_Data-Serialization/Unsolved/controllers/dish-routes.js
@@ -4,8 +4,12 @@ const Dish = require('../models/Dish');
 
 router.get('/', async (req, res) => {
   // TODO: Build out this route so that it serializes all of the dish objects that it receives. See the 'get' route below for a hint.
+  const dishData = await Dish.findAll().catch((err) => { 
+    res.json(err);
 });
-
+      const dishes = dishData.map((dish) => dish.get({ plain: true }));
+      res.render('all', { dishes });
+});
 // route to get one dish
 router.get('/dish/:id', async (req, res) => {
   try{ 
