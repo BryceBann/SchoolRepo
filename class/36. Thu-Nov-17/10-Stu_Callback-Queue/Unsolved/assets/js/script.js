a18248d9adb1bc0f4efc00a5db9c53f0a16b5122
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-11-17 21:34:09 -0600

    end of class

--- class/36. Thu-Nov-17/10-Stu_Callback-Queue/Unsolved/assets/js/script.js
+++ class/36. Thu-Nov-17/10-Stu_Callback-Queue/Unsolved/assets/js/script.js
@@ -1,9 +1,5 @@
-// function to block the stack for x number of milliseconds
-const pause = milliSeconds => {
-  // get the current time
-  var startTime = new Date().getTime(); 
-  // block stack until time's up
-  while (new Date().getTime() < startTime + milliSeconds); 
-}
-
-pause(8000);
+setTimeout(() => {
+  // remove the class that hides the element
+  document.querySelector('form button')
+    .classList.remove('display-none');
+}, 8000);
