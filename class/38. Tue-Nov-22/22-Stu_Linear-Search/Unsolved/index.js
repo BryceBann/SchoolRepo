0723bacef925504e8d802a219452e8aa00d411d4
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-11-22 21:48:27 -0600

    end of class

--- class/38. Tue-Nov-22/22-Stu_Linear-Search/Unsolved/index.js
+++ class/38. Tue-Nov-22/22-Stu_Linear-Search/Unsolved/index.js
@@ -1,4 +1,35 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7];
 
 // TODO: Complete the linearSearch function below.
-const linearSearch = (array, element) => {};
+const linearSearch = (array, element) => {
+for (let index = 0; index < array.length; index++) {
+    if (array[index] === element) {
+        return index;
+    }
+}
+
+return -1;
+};
+
+console.log(linearSearch(arr, 77));
+
+const linearSearchMultipleElements = (array, element) => {
+    const elemIndexes = [];
+  
+    for (let index = 0; index < array.length; index++) {
+      if (array[index] === element) {
+        elemIndexes.push(index);
+      }
+    }
+  
+    if (elemIndexes.length === 0) {
+      return 'Not found';
+    } else {
+      return elemIndexes;
+    }
+  };
+  
+  console.log(linearSearchMultipleElements(arr, 77));
+  
+  module.exports = { linearSearch, linearSearchMultipleElements };
+  
