c56360370f5db7b9dbbaa7e360a2656311a38f1e
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-11-30 15:03:26 -0600

    worked on algo

--- class/Algorithms/02-Algorithms/03-sum-array/Unsolved/sum-array.js
+++ class/Algorithms/02-Algorithms/03-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,12 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+var sumArray = function(arr) {
+    // console.log(arr)
+    let sum = 0
+    for(i = 0; i < arr.length; i++) {
+      console.log(arr[i])
+    //   sum = arr[i] + sum
+      sum += arr[i]
+    }
+    return sum
+};
