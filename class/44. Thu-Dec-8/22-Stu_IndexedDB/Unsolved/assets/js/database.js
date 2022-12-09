c23f443528fa3a2f58c9b5bff5eadcc819c88cdf
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-12-08 21:30:33 -0600

    end of class

--- class/44. Thu-Dec-8/22-Stu_IndexedDB/Unsolved/assets/js/database.js
+++ class/44. Thu-Dec-8/22-Stu_IndexedDB/Unsolved/assets/js/database.js
@@ -3,15 +3,17 @@ import { openDB } from 'idb';
 const initdb = async () =>
 
 // TODO: Add a comment explaining what this method does
-
+// we are creating a db as well as naming and delcation version
   openDB('todos', 1, {
     // TODO: Add a comment explaining the functionality of this method:
+    // will not create a db if the database is all ready created
     upgrade(db) {
       if (db.objectStoreNames.contains('todos')) {
         console.log('todos database already exists');
         return;
       }
       // TODO: Add a comment explaining what we're doing with the object store:
+      // creating a new obect that will increment and assiagn keyId
       db.createObjectStore('todos', { keyPath: 'id', autoIncrement: true });
       console.log('todos database created');
     },
--- class/44. Thu-Dec-8/24-Stu_IndexedDB-CRUD/Unsolved/assets/js/database.js
+++ class/44. Thu-Dec-8/24-Stu_IndexedDB-CRUD/Unsolved/assets/js/database.js
@@ -49,7 +49,7 @@ export const getOneDb = async (id) => {
 export const deleteDb = async (id) => {
   console.log('DELETE from the database', id);
   const todosDb = await openDB('todos', 1);
-  const tx = todosDb.transaction('todos', 'readonly');
+  const tx = todosDb.transaction('todos', 'readwrite');
   const store = tx.objectStore('todos');
   const request = store.delete(id);
   const result = await request;
@@ -66,6 +66,7 @@ export const putDb = async (id, content) => {
   const request = store.put({ id: id, todo: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
+  return result
 };
 
 initdb();
