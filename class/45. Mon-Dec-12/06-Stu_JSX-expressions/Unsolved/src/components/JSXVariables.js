be228f4d016d72e7e4809cf5ce52c1837ef004b6
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-12-12 21:30:56 -0600

    end of class

--- class/45. Mon-Dec-12/06-Stu_JSX-expressions/Unsolved/src/components/JSXVariables.js
+++ class/45. Mon-Dec-12/06-Stu_JSX-expressions/Unsolved/src/components/JSXVariables.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 // TODO: Create a `name` variable
-
+const name = "Bryce the man Bann"
 // TODO: Create a `thoughts` variable
+const thoughts = "Will be a very helpful library for making reactive webpages"
 // This should be a string regarding what you think of React
 
 function JSXVariables() {
@@ -11,11 +12,11 @@ function JSXVariables() {
       <div className="container">
         <div className="jumbotron">
           {/* TODO: Add your name variable */}
-          <h1>Hi! My name is (insert name here)</h1>
+          <h1>Hi! My name is {name}</h1>
           {/* TODO: Add your number of letters variable */}
-          <h2>My name has (insert number of letters in name here) letters</h2>
+          <h2>My name has {Math.count(name)} letters</h2>
           {/* TODO: Add your thoughts variable */}
-          <h2>I think React (insert thoughts about React here)</h2>
+          <h2>I think React {thoughts}</h2>
         </div>
       </div>
     </div>
