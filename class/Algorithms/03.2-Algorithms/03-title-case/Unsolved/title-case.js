cf1a2a730fcb8fe78fb60d8108e636448d0fbab3
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-12-15 14:03:09 -0600

    end of tutor

--- class/Algorithms/03.2-Algorithms/03-title-case/Unsolved/title-case.js
+++ class/Algorithms/03.2-Algorithms/03-title-case/Unsolved/title-case.js
@@ -1,3 +1,12 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function(str) {
+    const words = str.split(" ")
+    const newSentence = []
+for( let word of words) {
+    const wordArr = word.split("")
+    wordArr[0] = wordArr[0].toUpperCase()
+    newSentence.push(wordArr.join(""))
+}
+return newSentence.join(" ")
+};
