b2471fb9e5aa0c14c4c3a54ae5156cab664c6c8f
BryceBann <109399715+BryceBann@users.noreply.github.com>
2023-01-10 15:53:56 -0600

    mini project

--- class/practice-app/src/utils/CarContext.js
+++ class/practice-app/src/utils/CarContext.js
@@ -1,30 +1,8 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import { Provider } from 'react-redux';
 
-// Create our car context using React.CreateContext()
-export const CarContext = React.createContext();
+import store from './store';
 
-// Create a custom hook that allows easy access to our CarContext values
-export const useCar = () => useContext(CarContext);
-
-// Creating our car provider. Accepts an argument of `props`
 export default function CarProvider(props) {
-  const cars = [
-    {
-      id: 1,
-      make: 'Honda',
-      model: 'Civic',
-      year: '2008',
-      isRunning: false,
-    },
-    {
-      id: 2,
-      make: 'Tesla',
-      model: 'Y',
-      year: '2021',
-      isRunning: false,
-    },
-  ];
-
-  // The provider component will wrap all other components inside of it that need access to our global state
-  return <CarContext.Provider value={{ cars }} {...props} />;
+  return <Provider store={store} {...props} />;
 }
